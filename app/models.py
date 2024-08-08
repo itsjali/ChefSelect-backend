87abdbad6bb82d558e8367b07fdf7cac91beab20
jali <josh.ali@zerodeposit.com>
2024-08-08 21:06:51 +0100

    9 - Define the DB structure of the Recipes model and configure db in config and testing

---
+++ app/models.py
@@ -0,0 +1,24 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.String(1000), nullable=False)
+    ingredients = db.relationship("Ingredient", backref="recipe", lazy=True)
+    instructions = db.relationship("Instruction", backref="recipe", lazy=True)
+
+
+class Ingredient(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    unit = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    recipe_id = db.Column(db.Integer, db.ForeignKey("recipe.id"), nullable=False)
+
+
+class Instruction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    step = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
